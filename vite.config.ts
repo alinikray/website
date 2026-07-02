d1ef0f4240a7ed33b747967778228a18a571a5cf
alinikray <alinikray26@gmail.com>
2026-07-03 03:08:57 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: true, sb-2b4462m7qe2d.vercel.run
     port: 5173
   }
 })
